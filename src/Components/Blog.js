31b65c4e2799aef87a8c55601f2133013409c7fb
ifazzzz <shoaibahmedifaz@gmail.com>
2022-10-12 00:21:26 +0600

    blog added

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -3,7 +3,26 @@ import React from 'react';
 const Blog = () => {
     return (
         <div>
-            
+           <section className="bg-gray-100 text-gray-800">
+	<div className="container flex flex-col justify-center px-4 py-8 mx-auto md:p-8">
+		<h2 className="text-2xl font-semibold sm:text-4xl">Frequently Asked Questions</h2>
+		<p className="mt-4 mb-8 text-gray-600"></p>
+		<div className="space-y-4">
+			<details className="w-full border rounded-lg">
+				<summary className="px-4 py-6 focus:outline-none focus-visible:ring-cyan-600">What is the purpose of React Router?</summary>
+				<p className="px-4 py-6 pt-0 ml-4 -mt-4 text-gray-600">ReactJS Router is mainly used for developing Single Page Web Applications. React Router is used to define multiple routes in the application. When a user types a specific URL into the browser, and if this URL path matches any 'route' inside the router file, the user will be redirected to that particular route.React Router is a standard library system built on top of the React and used to create routing in the React application using React Router Package. It provides the synchronous URL on the browser with data that will be displayed on the web page. It maintains the standard structure and behavior of the application and mainly used for developing single page web applications.</p>
+			</details>
+			<details className="w-full border rounded-lg">
+				<summary className="px-4 py-6 focus:outline-none focus-visible:ring-cyan-600">How Context API works ?</summary>
+				<p className="px-4 py-6 pt-0 ml-4 -mt-4 text-gray-600">Context API is a (kind of) new feature added in version 16.3 of React that allows one to share state across the entire app (or part of it) lightly and with ease.React.createContext() is all you need. It returns a consumer and a provider. Provider is a component that as it's names suggests provides the state to its children. It will hold the "store" and be the parent of all the components that might need that store. Consumer as it so happens is a component that consumes and uses the state. </p>
+			</details>
+			<details className="w-full border rounded-lg">
+				<summary className="px-4 py-6 focus:outline-none focus-visible:ring-cyan-600">What is useRef hook in react.js and what is it's purpose?</summary>
+				<p className="px-4 py-6 pt-0 ml-4 -mt-4 text-gray-600">useRef returns a mutable ref object whose .current property is initialized to the passed argument (initialValue). The returned object will persist for the full lifetime of the component.However, useRef() is useful for more than the ref attribute. It’s handy for keeping any mutable value around similar to how you’d use instance fields in classes.This works because useRef() creates a plain JavaScript object. The only difference between useRef() and creating a  object yourself is that useRef will give you the same ref object on every render.</p>
+			</details>
+		</div>
+	</div>
+</section> 
         </div>
     );
 };
--- src/Components/Statistics.js
+++ src/Components/Statistics.js
@@ -1,13 +1,35 @@
 import React,{ useContext} from 'react';
 import { TopicsContext } from '../Layout/Main';
-import Chart from './Chart';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 const Statistics = () => {
-    // const topics= useContext(TopicsContext);
-
+    const topics = useContext(TopicsContext)
+    console.log(topics);
     return (
         <div>
-            <Chart></Chart>
+            <div className="container my-48 mx-auto">
+        <ResponsiveContainer width="100%" aspect={3}>
+        <BarChart
+          width={500}
+          height={300}
+          data={topics}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis dataKey="total"/>
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="total" fill="#8884d8" />
+          
+        </BarChart>
+      </ResponsiveContainer>
+      </div>
         </div>
     );
 };
