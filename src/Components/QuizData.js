6c6ddeb00ff1d547e023e7a3e5e5dfb167121874
ifazzzz <shoaibahmedifaz@gmail.com>
2022-10-11 20:52:18 +0600

    taost added

--- src/Components/QuizData.js
+++ src/Components/QuizData.js
@@ -1,35 +1,100 @@
 import React from 'react';
-import { EyeIcon } from '@heroicons/react/24/solid'
+import { EyeIcon } from '@heroicons/react/24/solid';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
 const QuizData = ({quiz}) => {
-    const {options, question, correctAnswe} = quiz;
-    const [q1, q2, q3, q4] = options;
-    console.log(options);
+    const {options, question, correctAnswer} = quiz;
+    // console.log(correctAnswer);
+    // console.log(correctAnswer);
+    const [op1, op2, op3, op4] = options;
+    // console.log(options);
+    let result;
+
+    const checkAnswer = (answer) => {
+       if (answer === correctAnswer) {
+        result = true;
+       }
+       else{
+        result = false;
+       }
+       result ? (toast.success("Correct Answer", {
+        position: "top-center",
+        autoClose: 1000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        })) : (toast.error("wrong Answer", {
+          position: "top-center",
+          autoClose: 1000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+          }))
+      
+    }
+
+const showToastMessage = () => {
+  toast.info(correctAnswer, {
+    position: "top-center",
+    autoClose: 1000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+    });
+}
     return (
+
         <div className="p-10 bg-gray-100">
             <p>{question}</p>
             <div className="p-6 my-12 grid grid-cols-1 gap-2">
                 <div>
-                  <input type="radio" value={q1} name="option"/>{q1}
+                  <button onClick={() => checkAnswer(op1)}><input type="radio" value={op1} name="option" />{op1}</button>
                 </div>             
                 <div>
-                  <input type="radio" value={q2} name="option"/>{q2}
+                  <button onClick={() => checkAnswer(op2)}><input type="radio" value={op2} name="option" />{op2}</button>
                 </div>
                 <div>
-                  <input type="radio" value={q3} name="option"/>{q3}
+                  <button onClick={() => checkAnswer(op3)}><input type="radio" value={op3} name="option" />{op3}</button>
                 </div>
                 <div>
-                  <input type="radio" value={q4} name="option"/>{q4} 
+                 <button onClick={() => checkAnswer(op4)}><input type="radio" value={op4} name="option" />{op4}</button>
                 </div>
             </div>
             <div className="flex justify-center items-center">
               <div>
                 <p>View The Answer : </p>
               </div>
+              <button onClick={showToastMessage}>
               <div className="w-5 h-5">
                 <EyeIcon></EyeIcon>
               </div>
+              </button>
             </div>
+            <ToastContainer
+            position="top-center"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="light"
+            />
         </div>
+         
     );
 };
 
